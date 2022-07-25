885eec37b1d64896bbe6ec02c6c8589ce802e2d3
EmilioAcostaG <103851807+EmilioAcostaG@users.noreply.github.com>
2022-07-25 01:02:02 -0400

    Code is working, almost ready for final submission

--- Assets/script.js
+++ Assets/script.js
@@ -8,81 +8,67 @@ function writePassword() {
   // write function generatePassword, needs to return a string
   function generatePassword () {
 
-    var numOfCharacters = prompt("How many characters would you like to include in your password? At leaset 8 and no more than 128 characters.");
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var upperCase = letters.map(element => {
+      return element.toUpperCase();
+    }); // created this function to turn letter uppercase, tried: letter.toUpperCase but it was not accepting it
+    var numbers = ["0", "1", "3", "4", "5", "6", "7", "8", "9"];
+    var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+    var list = [] // this is where the characters that make up the password will be added to
 
-    // if (numOfCharacters === null) {
-     // return;
-  
-   // }
+    // to define length of password
+    var numOfCharacters = prompt("How many characters would you like to include in your password? Please enter at leaset 8 and no more than 128 characters.");
 
-   var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-   var numbers = ["0", "1", "3", "4", "5", "6", "7", "8", "9"];
-   var specials = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+    // will cancel prompt if left blank
+    if (numOfCharacters === null) {
+      return "No Password Generated" //why is it still saying undefined
+    }
 
-
-
-
+    // will tell user to enter new number if a 0 or number greater than 128 is entered
     if (numOfCharacters < 8 || numOfCharacters > 128) {
-      alert ("Change number of characters.")
+      alert ("The numer of characters entered is not an option. \nPlease enter a different number of characters.")
     }
 
+    //otherwise, following will as to confirm for password criteria.
+    // nothing extra needed for false, simply means not to include in password
     else {
+      var lowerCase = confirm("Click OK if you would like to include lowercase letters.");
 
+       if (lowerCase === true) {
+        list = list.concat(letters);
+      }
 
-      var lowerCase = confirm("Click OK if you would like to include lowercase characters.");
+      var upper = confirm("Click OK if you would like to include uppercase letters.");
+      if (upper === true) {
+        list = list.concat(upperCase);
+      }
 
-      var upperCase = confirm("Click OK if you would like to include uppercase characters.")
-      var numbers = confirm("Click OK if you would like to include numeric characters.")
-      var specialCharacters = confirm("Click OK if you would like to include special characters in your password.");
+      var numeric = confirm("Click OK if you would like to include numeric characters.");
+      if (numeric === true) {
+        list = list.concat(numbers);
+      }
 
+      var specials = confirm("Click OK if you would like to include special characters in your password.");
+      if (specials === true) {
+        list = list.concat(specialChar);
+      }
 
+      var charList = []
 
+      // loop is needed to repeat a random selection of characters
+      // it will be repeated based on the input from "numOfCharacters" prompt
+      for (var i = 0; i < numOfCharacters; i++) {
+    
+        var index = Math.floor(Math.random() * list.length); //this creates a random number
+        var char = list [index] 
 
+        charList = charList + char
+  }
 
+      return charList
 
-
-
-
-
-
-
-
-      var index = Math.floor(Math.random() * numOfCharacters);
-     // var 
     }
     
-
-
-    
-
-
-
-
-  
-
-
-   
-
-
-
-
-
-    
-
-    
-
-   
-    
-    // var answer = prompt("How many characters would you like to include in your password?"); //enter a number
-    //var answer = confirm("Click OK if you would like to include lowercase characters."); // true or false
-    //var answer = confirm("Click OK if you would like to include uppercase characters."); // true or false
-
-    //var answer = confirm("Click OK if you would like to include numeric characters."); // true or false
-
-   // var answer = confirm("Click OK if you would like to include special characters in your password.");
-
-
-  
   }
   
   
@@ -91,9 +77,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
